c401b69fd224df61dad7f8221e6fc6a424fd0405
Danny Cairney <sharkmandan@users.noreply.github.com>
2016-01-11 11:01:52 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {};
 config.app = {};
 config.app.port = process.env.PORT || 3000;
 config.app.fromAddress = process.env.FROM_ADDRESS || "resumator@mdstaff.com";
-config.app.toAddress = process.env.TO_ADDRESS || "dcairney@mdstaff.com";
+config.app.toAddress = process.env.TO_ADDRESS || "asm.hr.dev@mdstaff.com";
 
 config.sendgrid = {};
 config.sendgrid.username = process.env.SENDGRID_USERNAME || "mdstaff";
